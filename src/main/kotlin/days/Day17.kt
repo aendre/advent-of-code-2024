31a68ae432fee6a5baceb8dbd020b31431446cfb
eandras <aendre@users.noreply.github.com>
2024-12-22 14:54:03 +0100

    Day 17 (part2)

--- src/main/kotlin/days/Day17.kt
+++ src/main/kotlin/days/Day17.kt
@@ -2,7 +2,7 @@ package days
 import utils.*
 import kotlin.math.pow
 
-fun executeAll(registers: List<Long>, program: List<Int>): MutableList<Long> {
+fun executeAll(registers: List<Long>, program: List<Long>): MutableList<Long> {
   var (regA, regB, regC) = registers;
 
   fun getComboOperand(value: Long): Long {
@@ -20,26 +20,24 @@ fun executeAll(registers: List<Long>, program: List<Int>): MutableList<Long> {
     var pointer = 0
 
     fun op0(comboOperand:Long) = (regA / (2.toDouble().pow(comboOperand.toDouble()))).toLong()
-
     while (pointer < program.size) {
       val opcode = program[pointer]
       val literalOperand = program[pointer+1].toLong()
       val comboOperand = getComboOperand(literalOperand)
 
       when (opcode) {
-        0 -> regA = op0(comboOperand)
-        1 -> regB = regB xor literalOperand
-        2 -> regB = comboOperand % 8
-        3 -> if (regA != 0L) { pointer = literalOperand.toInt(); continue }
-        4 -> regB = regB xor regC
-        5 -> output.add(comboOperand % 8)
-        6 -> regB = op0(comboOperand)
-        7 -> regC = op0(comboOperand)
+        0L -> regA = op0(comboOperand)
+        1L -> regB = regB xor literalOperand
+        2L -> regB = comboOperand % 8
+        3L -> if (regA != 0L) { pointer = literalOperand.toInt(); continue }
+        4L -> regB = regB xor regC
+        5L -> output.add(comboOperand % 8)
+        6L -> regB = op0(comboOperand)
+        7L -> regC = op0(comboOperand)
         else -> throw Throwable("Not possible")
       }
       pointer += 2
     }
-
     return output
   }
 
@@ -49,25 +47,23 @@ fun executeAll(registers: List<Long>, program: List<Int>): MutableList<Long> {
 suspend fun main() = AdventOfCode(day = 17, year = 2024) {
   val (registers, program) = input.split("\n\n").toPair().let {
     it.first.replace("Register [ABC]: ".toRegex(),"").lines().map { it.toLong() } to
-    it.second.replace("Program: ","").split(",").map { it.toInt() }
+    it.second.replace("Program: ","").split(",").map { it.toLong() }
   }
 
   part1 = executeAll(registers,program).joinToString(",")
 
-  for (a in 0..10_000_000_000L) {
-    val modifiedRegisters = listOf(a) + registers.drop(1)
-    if (executeAll(modifiedRegisters,program).joinToString(",") == program.joinToString(",")) {
-      part2 = a
-      break
+  fun solvePart2(program: List<Long>, target: List<Long>): Long {
+    var registerA = if (target.size == 1) {
+      0
+    } else {
+      8 * solvePart2(program, target.drop(1))
     }
+    while(executeAll(listOf(registerA) + registers.drop(1),program) != target) {
+      registerA++
+    }
+    return registerA
   }
 
-}.start()
+  part2 = solvePart2(program,program)
 
-//val example = """
-//Register A: 1024
-//Register B: 0
-//Register C: 0
-//
-//Program: 0,3,5,4,3,0
-//""".trimIndent()
+}.start()
