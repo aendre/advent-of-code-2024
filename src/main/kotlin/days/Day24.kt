e2a6b675bda0874ff33e6c25816bcd9f3b9fee32
eandras <aendre@users.noreply.github.com>
2024-12-27 01:36:40 +0100

    Day 24

--- src/main/kotlin/days/Day24.kt
+++ src/main/kotlin/days/Day24.kt
@@ -1,6 +1,32 @@
 package days
 import utils.*
 
+fun toDotFile(wires:Map<String,Int>, gates: List<List<String>>) {
+    var out = """
+digraph g {
+  fontname="Helvetica,Arial,sans-serif"
+  node [fontname="Helvetica,Arial,sans-serif"]
+  edge [fontname="Helvetica,Arial,sans-serif" fontsize="8"]
+""";
+    wires.forEach {
+        out += "  node [fillcolor=\"white\" style=\"filled\"] ${it.key}; \n"
+    }
+    gates.forEach {
+        val (in1,op,in2,outwire) = it
+        if (op=="XOR") out += "  node [fillcolor=\"red\" style=\"filled\"] ${outwire}; \n"
+        if (op=="AND") out += "  node [fillcolor=\"blue\" style=\"filled\"] ${outwire}; \n"
+        if (op=="OR") out += "  node [fillcolor=\"green\" style=\"filled\"] ${outwire}; \n"
+    }
+    gates.forEach {
+        val (in1,op,in2,outwire) = it
+        out += "$in1 -> $outwire  [headlabel=$op]\n"
+        out += "$in2 -> $outwire  [headlabel=$op]\n"
+    }
+
+    out+="\n }"
+    writeContent("build/cache/input-24.dot", out)
+}
+
 suspend fun main() = AdventOfCode(day = 24, year = 2024) {
     val (initialWires, gates) = input.split("\n\n").toPair().let {
         it.first.lines().map { it.split(": ").toPair().let { it.first to it.second.toInt() } } to
@@ -29,5 +55,6 @@ suspend fun main() = AdventOfCode(day = 24, year = 2024) {
         .toList().sortedBy { it.first }.reversed()
         .map { it.second }.joinToString("").toLong(2)
 
-
+    toDotFile(initialWires.toMap(),gates)
+    part2 = listOf("z12", "kwb","z24","tgr","z16","qkf","jqn","cph").sorted().joinToString(",")
 }.start()
